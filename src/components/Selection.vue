14db33084cd1c561a467f44eb7597053ef1b64e9
KiditsGood <71182254+KiditsGood@users.noreply.github.com>
2022-10-15 17:19:19 +0300

    test

---
+++ src/components/Selection.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="selection__block">
+        <div class="selection__top">
+            <div @click="removeItem(item)" class="selection__item" v-for="item in selectedItems" :key="item.id">
+                <span>{{ item.name }}</span>
+            </div>
+            <span class="counter" v-if="counter">{{ selectedItems.length }}/{{ maxItems }}</span>
+        </div>
+        <div class="selection__bottom">
+            <div @click="selectItem(item)" class="selection__item" v-for="item in items" :key="item.id">
+                <span>{{ item.name }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            items: Array,
+            maxItems: {
+                type: Number,
+                default: 6
+            },
+            counter: {
+                type: Boolean,
+                default: false
+            }
+        },
+        data() {
+            return {
+                selectedItems: []
+            }
+        },
+        methods: {
+            selectItem(item) {
+                if (this.maxItems === this.selectedItems.length) return
+
+                this.selectedItems.push(item)
+            },
+            removeItem(item) {
+                this.selectedItems.splice(this.selectedItems.indexOf(item), 1)
+            }
+        }
+    }
+</script>
--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -1,53 +1,16 @@
 <template>
     <main class="wrapper">
-        <div class="getters">
-            <div class="getters--container">
-                <AppGetter v-for="userCheck in userAlready" :id="userCheck.id" :name="userCheck.name" :key="userCheck.id"/>
-                <div class="getters--counter">
-                    <p>{{ userAlready.length }}</p>
-                    <p>/</p>
-                    <p>6</p>
-                </div>
-            </div>
-            <div class="getters--container">
-                <AppGetter v-for="allCheck in allAlready" :id="allCheck.id" :name="allCheck.name" :key="allCheck.id"/>
-            </div>
-        </div>
-        <div class="setters">
-            <div class="setters--container">
-                <AppSetter
-                        v-model="isChecked"
-                        @click.native="userHandler(userItem)"
-                        v-for="userItem in userSelect"
-                        :id="userItem.id"
-                        :name="userItem.name"
-                        :key="userItem.id"
-                />
-            </div>
-            <div class="setters--container">
-                <AppSetter
-                        v-model="isChecked"
-                        @click.native="allHandler(allItem)"
-                        v-for="allItem in allSelect"
-                        :id="allItem.id"
-                        :name="allItem.name"
-                        :key="allItem.id"
-                />
-            </div>
-        </div>
+        <Selection :items="leftItems" :counter="true" />
+        <Selection :items="rightItems" :maxItems="1" />
     </main>
 </template>
 
 <script>
-    import AppGetter from "@/components/AppGetter";
-    import AppSetter from "@/components/AppSetter";
+    import Selection from '@/components/Selection'
     export default {
-        name: "AppHome",
-        components: {AppSetter, AppGetter},
-
         data() {
             return {
-                userSelect: [
+                leftItems: [
                     {
                         "id": 1,
                         "name": "Shoes 1"
@@ -81,7 +44,7 @@
                         "name": "T-shirt 4"
                     }
                 ],
-                allSelect: [
+                rightItems: [
                     {
                         "id": 11,
                         "name": "Jacket 1"
@@ -115,92 +78,13 @@
                         "name": "Hoodie 4"
                     }
                 ],
-                userAlready: [],
-                allAlready: [],
-                isChecked: false
             }
         },
-
+        components: {
+            Selection
+        },
         methods: {
-            userHandler(currElem) {
-                for (let i = 0; i < this.userAlready.length; i++) {
-                    if (this.userAlready[i].id === currElem.id) {
-                        this.isChecked = true
-                    }
-                }
 
-                if (this.userAlready.length < 6) {
-                    if (this.isChecked) {
-                        this.userAlready = this.userAlready.filter((el) => {return el.id !== currElem.id})
-
-                        this.isChecked = false
-                    }
-
-                    else {
-                        this.userAlready.push(currElem)
-                    }
-                }
-
-                if (this.userAlready.length === 5) {
-                    
-                }
-            },
-
-            allHandler(currElem) {
-                let allHandlerItems = this.allAlready
-
-                allHandlerItems.length = 0
-                allHandlerItems.push(currElem)
-            }
         }
     }
 </script>
-
-<style lang="scss" scoped>
-    .wrapper{
-        max-width: 1440px;
-        margin: 100px auto;
-        display: flex;
-        flex-direction: column;
-        gap: 50px;
-    }
-    .setters{
-        display: flex;
-        justify-content: space-between;
-
-        &--container{
-            display: flex;
-            justify-content: center;
-            flex-wrap: wrap;
-            border: 1px solid seagreen;
-            border-radius: 10px;
-            padding: 25px;
-            width: 500px;
-            gap: 10px;
-        }
-    }
-    .getters{
-        display: flex;
-        justify-content: space-between;
-        position: relative;
-
-        &--container{
-            display: flex;
-            justify-content: center;
-            flex-wrap: wrap;
-            border: 1px solid seagreen;
-            border-radius: 10px;
-            padding: 25px;
-            width: 500px;
-            gap: 10px;
-        }
-
-        &--counter{
-            display: flex;
-            align-items: center;
-            gap: 5px;
-            position: absolute;
-            bottom: 5px;
-        }
-    }
-</style>
